38ee1b7eab0555528b093c0d0158783d56b39974
bruxarch <bruno.red.mem@gmail.com>
2025-06-20 23:56:14 -0300

    test actions 6

--- src/test/java/com/redolfi/demolfi/DemolfiApplicationTests.java
+++ src/test/java/com/redolfi/demolfi/DemolfiApplicationTests.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-@ActiveProfiles("test")  // <- Esto fuerza a usar application-test.properties
 class DemolfiApplicationTests {
 
 	@Test
